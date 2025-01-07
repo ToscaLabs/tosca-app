329b35ac2912d1c45f4976dcda97580a56c7ec4c
Luni-4 <luni-4@hotmail.it>
2025-01-08 00:13:42 +0100

    Wip

--- src/index.rs
+++ src/index.rs
@@ -2,6 +2,9 @@ use serde::Serialize;
 
 use std::sync::Arc;
 
+use ascot_library::device::DeviceKind;
+use ascot_library::hazards::{Category, Hazards};
+
 use axum::extract::State;
 use axum::http::StatusCode;
 use axum::response::Html;
@@ -12,17 +15,36 @@ use crate::{AppState, TITLE};
 
 #[derive(Serialize)]
 struct Device {
-    title: &'static str,
+    kind: DeviceKind,
 }
 
 impl Device {
-    fn new() -> Self {
-        Self { title: "hello" }
+    fn new(kind: DeviceKind) -> Self {
+        Self { kind }
     }
 }
 
 fn create_devices() -> Vec<Device> {
-    vec![Device::new()]
+    vec![
+        Device::new(DeviceKind::Light),
+        Device::new(DeviceKind::Camera),
+    ]
+}
+
+#[derive(Serialize)]
+struct HazardData {
+    kind: DeviceKind,
+}
+
+fn create_hazards() -> Vec<HazardData> {
+    let mut HazardData = Vec::new();
+    let mut safety = Hazards::init_with_elements(Category::Safety.hazards());
+    let privacy = Hazards::init_with_elements(Category::Privacy.hazards());
+    let financial = Hazards::init_with_elements(Category::Financial.hazards());
+
+    safety.merge(&privacy);
+    safety.merge(&financial);
+    safety
 }
 
 pub(crate) async fn index(State(state): State<Arc<AppState>>) -> Result<Html<String>, StatusCode> {
@@ -31,8 +53,10 @@ pub(crate) async fn index(State(state): State<Arc<AppState>>) -> Result<Html<Str
     let rendered = template
         .render(context! {
             title => TITLE,
+            no_devices_message => "No devices found.",
             discover_message => "Discover device",
             devices => create_devices(),
+            hazards => create_hazards(),
         })
         .unwrap();
 
